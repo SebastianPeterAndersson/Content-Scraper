d09c54c48348a545a4607c87cb2098067a05eda6
Sebastian Andersson <sebastianpeterandersson@gmail.com>
2017-01-17 11:54:31 +0100

    Added modules with comments

--- scraper.js
+++ scraper.js
@@ -1,7 +1,11 @@
 var http = require('http'),
+
+    // For sending request to a website:
     request = require("request"),
+    // For scraping the website with jQuery-familiar syntax:
     cheerio = require("cheerio"),
     fs = require("fs"),
+    // For converting list of objects to a CSV file:
     json2csv = require('json2csv');
 
 
