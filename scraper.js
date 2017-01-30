bf5d2d7b9e868461332c550a6016190e8023677d
Sebastian Andersson <sebastianpeterandersson@gmail.com>
2017-01-30 22:57:42 +0100

    Final changes before commiting to github

--- scraper.js
+++ scraper.js
@@ -1,15 +1,18 @@
 var http = require('http'),
-
     // For sending request to a website:
     request = require("request"),
     // For scraping the website with jQuery-familiar syntax:
     cheerio = require("cheerio"),
-    fs = require("fs"),
+    fs = require('fs-extra'),
     // For converting list of objects to a CSV file:
     json2csv = require('json2csv');
 
+    var shirtArr = [];
+    var arr = [];
+    var fields = ["title", "price", "imgurl", "url", "time"];
 
-    // Create data directory if there already isn't one
-    if (!fs.existsSync("data")) {
-        fs.mkdirSync("data");
-    }
+
+
+
+
+console.log("Server running in the terminal");
